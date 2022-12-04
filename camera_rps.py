28496cc4530b74e2e659e31b71701cf71591dd56
BishrBinAli <bishrkc@gmail.com>
2022-12-04 11:17:58 +0000

    Repeat game till someone wins 3 rounds

--- camera_rps.py
+++ camera_rps.py
@@ -2,12 +2,13 @@ import cv2
 from keras.models import load_model
 import numpy as np
 import time
+import random
 
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
-fontColor = (0, 0, 0)
-thickness = 1
+fontColor = (255, 0, 0)
+thickness = 2
 lineType = 2
 
 
@@ -28,6 +29,35 @@ def get_prediction(image):
     label = options[np.argmax(prediction)]
     return label
 
+
+def get_computer_choice():
+    options = ["Rock", "Paper", "Scissors"]
+    choice = random.choice(options)
+    return choice
+
+
+def get_user_choice():
+    user_choice = input("Enter your choice(Rock, Paper, or Scissors):")
+    return user_choice
+
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        print("It is a tie!")
+        return "Tie"
+    elif (computer_choice == "Rock" and user_choice == "Scissors"):
+        print("You lost")
+        return "Computer"
+    elif (computer_choice == "Paper" and user_choice == "Rock"):
+        print("You lost")
+        return "Computer"
+    elif (computer_choice == "Scissors" and user_choice == "Paper"):
+        print("You lost")
+        return "Computer"
+    else:
+        print("You won!")
+        return "User"
+
 # def countdown(duration, delay=5):
 #     i = duration
 #     current_time = time.time()
@@ -39,10 +69,20 @@ def get_prediction(image):
 #             current_time = time.time()
 
 
-countdown = 10
-stop = False
+countdown = 5
+round_over = False
+game_over = False
 current_time = time.time()
-prediction_text = ""
+
+
+computer_wins = 0
+user_wins = 0
+
+user_text = ""
+computer_text = ""
+winner_text = ""
+result_text = ""
+
 while True:
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
@@ -51,8 +91,28 @@ while True:
                         127.0) - 1  # Normalize the image
     data[0] = normalized_image
 
-    img_txt = f"Show your choice in {countdown}"
-    cv2.putText(frame, img_txt,
+    countdown_txt = f"Show your choice in {countdown}"
+
+    if countdown == 0 and round_over == False:
+        countdown_txt = ""
+        user_choice = get_prediction(data)
+        if user_choice != "Nothing":
+            print(f"You chose {user_choice}")
+            user_text = f"You chose {user_choice}"
+            computer_choice = get_computer_choice()
+            computer_text = f"Computer chose {computer_choice}"
+            winner = get_winner(computer_choice, user_choice)
+            if winner == "Tie":
+                winner_text = f"It is a tie!"
+            else:
+                winner_text = f"{winner} wins this round!"
+                if winner == "Computer":
+                    computer_wins += 1
+                if winner == "User":
+                    user_wins += 1
+            round_over = True
+
+    cv2.putText(frame, countdown_txt,
                 (10, 30),
                 font,
                 fontScale,
@@ -66,14 +126,7 @@ while True:
             print(countdown)
             current_time = time.time()
 
-    if countdown == 0 and stop == False:
-        prediction = get_prediction(data)
-        if prediction != "Nothing":
-            print(f"You chose {prediction}")
-            prediction_text = f"You chose {prediction}"
-            stop = True
-
-    cv2.putText(frame, prediction_text,
+    cv2.putText(frame, user_text,
                 (10, 60),
                 font,
                 fontScale,
@@ -81,10 +134,83 @@ while True:
                 thickness,
                 lineType)
 
+    cv2.putText(frame, computer_text,
+                (10, 90),
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+    cv2.putText(frame, winner_text,
+                (10, 120),
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+    score_text = f"User: {user_wins},    Computer: {computer_wins}"
+    cv2.putText(frame, score_text,
+                (10, 400),
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+    if round_over == True:
+        if user_wins > 2 or computer_wins > 2:
+            if user_wins > 2:
+                final_winner = "User"
+            if computer_wins > 2:
+                final_winner = "Computer"
+            result_text = f"{final_winner} is the winner! \n Press r to restart game \n Press q to quit"
+            game_over = True
+        else:
+            result_text = "Press n to start next round \n Press q to quit"
+
+    y0, dy = 200, 30
+    for i, line in enumerate(result_text.split('\n')):
+        y = y0 + i*dy
+        # cv2.putText(img, line, (50, y ), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        cv2.putText(frame, line,
+                    (90, y),
+                    font,
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    if round_over == True:
+        pressed_key = cv2.waitKey(0)
+        if pressed_key == ord('n'):
+            countdown = 5
+            result_text = ""
+            user_text = ""
+            computer_text = ""
+            winner_text = ""
+            round_over = False
+        if pressed_key == ord('q'):
+            break
+
+    if game_over == True:
+        pressed_key = cv2.waitKey(0)
+        if pressed_key == ord('r'):
+            countdown = 5
+            result_text = ""
+            user_text = ""
+            computer_text = ""
+            computer_wins = 0
+            user_wins = 0
+            game_over = False
+        if pressed_key == ord('q'):
+            break
 
 
 # After the loop release the cap object
